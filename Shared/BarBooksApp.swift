f686ab09a80dde5bff8bfcfdc61d3745ffe86dcd
Alex Furlotte <alexfurlotte10@gmail.com>
2021-01-20 12:20:35 -0400

    Initial Commit

---
+++ Shared/BarBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarBooksApp.swift
+//  Shared
+//
+//  Created by Alex Furlotte on 2021-01-20.
+//
+
+import SwiftUI
+
+@main
+struct BarBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
